43f157a763b1c8e6cf076d0700f48811ed60b459
Stacey <stacey@snappymob.com>
2018-12-17 13:02:10 +0800

    Showing bottom fragment once the app show up, tapping back button will dismiss the bottom fragment

---
+++ app/src/main/java/com/example/fragmentconcept/BottomFragment.kt
@@ -0,0 +1,37 @@
+package com.example.fragmentconcept
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_bottom.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BottomFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class BottomFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textView.text = this.javaClass.simpleName
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = BottomFragment()
+    }
+}
--- app/src/main/java/com/example/fragmentconcept/MainActivity.kt
+++ app/src/main/java/com/example/fragmentconcept/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.fragmentconcept
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.abc_slide_in_bottom, R.anim.abc_slide_out_bottom, R.anim.abc_slide_in_bottom, R.anim.abc_slide_out_bottom)
+                .add(bottomFragmentContainer.id, BottomFragment.newInstance(), BottomFragment::class.java.simpleName)
+                .addToBackStack(BottomFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
