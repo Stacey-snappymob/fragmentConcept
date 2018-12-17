99955872fd0a585e0c36cb99553f350d79ba06c6
Stacey <stacey@snappymob.com>
2018-12-17 14:55:12 +0800

    Launch activity based on selection, implemented show bottom fragment with interaction

---
+++ app/src/main/java/com/example/fragmentconcept/BottomFragmentByInteractionActivity.kt
@@ -0,0 +1,26 @@
+package com.example.fragmentconcept
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_bottom_fragment_by_interaction.*
+
+class BottomFragmentByInteractionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_fragment_by_interaction)
+
+        goButton.setOnClickListener {
+            if (supportFragmentManager.fragments.isEmpty()) {
+                supportFragmentManager.beginTransaction()
+                    .setCustomAnimations(R.anim.abc_slide_in_bottom, R.anim.abc_slide_out_bottom, R.anim.abc_slide_in_bottom, R.anim.abc_slide_out_bottom)
+                    .add(bottomFragmentContainer.id, BottomFragment.newInstance(), BottomFragment::class.java.simpleName)
+                    .addToBackStack(BottomFragment::class.java.simpleName)
+                    .commit()
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/fragmentconcept/MainActivity.kt
+++ app/src/main/java/com/example/fragmentconcept/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.fragmentconcept
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragmentByDefaultButton.setOnClickListener {
+            val intent = Intent(this, BottomFragmentByDefaultActivity::class.java)
+            startActivity(intent)
+        }
+
+        fragmentWithInteractionButton.setOnClickListener {
+            val intent = Intent(this, BottomFragmentByInteractionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
